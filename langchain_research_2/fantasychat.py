066c69add444ce64f0223cf8c55bc9d1363f82ef
vishalshenoy <vishal.nshenoy@icloud.com>
2023-12-04 22:37:51 -0800

    new changes

--- langchain_research_2/fantasychat.py
+++ langchain_research_2/fantasychat.py
@@ -5,11 +5,10 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
+import os
 
 load_dotenv('.env')
-
-# OpenAI API key
-openai_key = "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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Function to load data from CSV file
 def load_data_from_csv(file_path):
@@ -17,10 +16,10 @@ def load_data_from_csv(file_path):
     return loader.load()
 
 # Setup the conversational chain
-def setup_chain(data, openai_key):
+def setup_chain(data, OPENAI_API_KEY):
     embeddings = OpenAIEmbeddings()
     vectors = FAISS.from_documents(data, embeddings)
-    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=openai_key),
+    chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=OPENAI_API_KEY),
                                                   retriever=vectors.as_retriever())
     return chain
 
@@ -34,7 +33,7 @@ def main():
     # Input CSV file path from the user
     csv_file_path = input("Enter the path to your CSV file: ")
     data = load_data_from_csv(csv_file_path)
-    chain = setup_chain(data, openai_key)
+    chain = setup_chain(data, OPENAI_API_KEY)
 
     history = []
     print("Hello! Ask me anything about your data.")
