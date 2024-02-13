b22fced4a64837f81947d8d01400d71bd663b389
vishalshenoy <vishal.nshenoy@icloud.com>
2024-02-12 20:59:08 -0800

    arnav sentiment analysis work

--- project/app.py
+++ project/app.py
@@ -4,6 +4,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders.json_loader import JSONLoader
+from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 import os
@@ -19,22 +20,37 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 loader = JSONLoader(file_path=json_path, jq_schema='.[]', text_content=False)
 data = loader.load()
 
+injury_loader = CSVLoader(file_path='injuryreports.csv', encoding="utf-8")
+injury_reports = injury_loader.load()
+
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 vectors = FAISS.from_documents(data, embeddings)
+injury_vectors = FAISS.from_documents(injury_reports, embeddings)
 
 chain = ConversationalRetrievalChain.from_llm(
     llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=OPENAI_API_KEY),
     retriever=vectors.as_retriever())
 
+injury_chain = ConversationalRetrievalChain.from_llm(
+    llm=ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo', openai_api_key=OPENAI_API_KEY),
+    retriever=injury_vectors.as_retriever())
+
 # Function to handle conversation
 def conversational_chat(query):
-    result = chain({"question": query, "chat_history": st.session_state['history']})
+    injury_query = f'{query}. What is the current situation towards this/these player(s) regarding injuries?' 
+    injury_report = injury_chain({"question": injury_query, "chat_history": st.session_state['injury_history']})
+
+    answer_query = f"{query}. Use the following as additional info to answer: {injury_report['answer']}"
+    result = chain({"question": answer_query, "chat_history": st.session_state['history']})
+
     st.session_state['history'].append((query, result["answer"]))
+
     return result["answer"]
 
 # Initialize session state
 if 'history' not in st.session_state:
     st.session_state['history'] = []
+    st.session_state['injury_history'] = []
 
 # Streamlit layout
 st.title("Fantasy Football Analyst Chatbot 🏈")
@@ -44,7 +60,6 @@ st.markdown("Ask me anything about fantasy football!")
 response_container = st.container()
 container = st.container()
 
-# Displaying chat history
 with response_container:
     for i in range(len(st.session_state['history'])):
         if i % 2 != 0:
@@ -59,10 +74,11 @@ with container:
         submit_button = st.form_submit_button(label='Ask the Analyst')
 
         # Prefix to set the persona of the chatbot
-        user_query = "You are an expert sport analyst specializing in Fantasy Football! Use the data provided and give confident predictions and analysis." + user_input
+        user_query = "You are an expert sport analyst specializing in Fantasy Football! Use the data provided to provide CONFIDENT advice (DO NOT hesitate about giving opinions)" + user_input
 
     if submit_button and user_input:
         output = conversational_chat(user_query)
         # Update the chat history and refresh the page to show the latest message at the top
         st.session_state['history'].append((user_input, output))
         st.experimental_rerun()
+    
